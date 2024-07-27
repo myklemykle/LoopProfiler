b6eadb8b15818d38f1a2a27cc67bf1b3f70d43c4
Mykle Hansen <mykle@mykle.com>
2024-07-27 14:56:19 -0700

    simpler!

--- examples/simple/simple.ino
+++ examples/simple/simple.ino
@@ -24,21 +24,14 @@ void setup() {
 
 
 void loop() { 
-	static elapsedMillis timer = 0;
-	static int resetCounter = 0;
-
 	PROFILE_LOOP(); 
 
 	PROFILE_START("delayRnd");
-
 	delayRnd();
-
 	PROFILE_END("delayRnd");
 
 	PROFILE_START("delay150");
-
 	delay150();
-
 	PROFILE_END("delay150");
 
 	PROFILE_PRINT_AVG(Serial); 
